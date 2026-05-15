24f46757054c995b0b70ccc933a95a818f307e7b
monte <lucmontero@gmail.com>
2026-05-15 16:01:56 -0300

    import não utilizado

--- src/main/java/com/lucas/couponapi/dto/CouponDTO.java
+++ src/main/java/com/lucas/couponapi/dto/CouponDTO.java
@@ -1,7 +1,6 @@
 package com.lucas.couponapi.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
